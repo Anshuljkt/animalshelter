2a99885cbb187de26f68512c83f3089935e57188
Alex Bates <alexb8z@lawn-143-215-115-117.lawn.gatech.edu>
2019-11-21 19:33:17 -0500

    This is the three large components for front end

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NavBar = () => (
+    <nav class="NavBar">
+        <p class="left">Benji's animal shelter</p>
+        <p class="mid">About</p>
+        <p class="right">Adopt a pet</p>
+    </nav>
+)
+
+export default NavBar
